fb8f9b4c89b59578fe96ac301e21840b52fb92e3
Lereko Foloko <84933354+Boithuto2021@users.noreply.github.com>
2022-10-04 07:07:13 +0200

    added framer motion libray

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
   return <div className="App">Learning framer motion</div>;
